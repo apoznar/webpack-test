26311ccec475417b1a1c52915dd8e57f379abb11
Adam Poznar <adampoznar@gmail.com>
2020-07-12 19:07:01 +0200

    usage of moment library

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 
+import moment from 'moment';
+
 /* place your code below */
 
 const firstName = "Adam";
@@ -23,3 +25,9 @@ hamburger.addEventListener("click", () => {
   const nav = document.querySelector(".navigation--js");
   nav.classList.toggle("navigation--open");
 });
+
+const startOfDay = moment().startOf('day').fromNow();
+
+const timePlaceholder = document.querySelector('.time--js');
+
+timePlaceholder.innerHTML = startOfDay;
